df4f8db6afe3a051b5a7fdacc247651587b06dc1
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-03-30 17:58:17 +0200

    Update translation, add support website URL

--- src/upload/admin/controller/extension/analytics/ps_google_analytics.php
+++ src/upload/admin/controller/extension/analytics/ps_google_analytics.php
@@ -7,9 +7,14 @@ class ControllerExtensionAnalyticsPsGoogleAnalytics extends Controller
     const EXTENSION_EMAIL = 'support@playfulsparkle.com';
 
     /**
-     * @var string The documentation URL for the extension.
+     * @var string The URL to the support website.
      */
-    const EXTENSION_DOC = 'https://github.com/playfulsparkle/oc3_google_analytics.git';
+    const SUPPORT_URL = 'https://support.playfulsparkle.com';
+
+    /**
+     * @var string The GitHub repository URL of the extension.
+     */
+    const GITHUB_REPO_URL = 'https://github.com/playfulsparkle/oc3_google_analytics';
 
     private $error = array();
 
@@ -82,7 +87,7 @@ class ControllerExtensionAnalyticsPsGoogleAnalytics extends Controller
             $data['analytics_ps_google_analytics_google_tag_id'] = (string) $this->model_setting_setting->getSettingValue('analytics_ps_google_analytics_google_tag_id', $this->request->get['store_id']);
         }
 
-        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::EXTENSION_EMAIL, self::EXTENSION_EMAIL, self::EXTENSION_DOC);
+        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::SUPPORT_URL, self::GITHUB_REPO_URL, self::EXTENSION_EMAIL);
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
